0d082f2a00e85350f9a103fd4b6700f774159db0
DariusBolos <63288528+DariusBolos@users.noreply.github.com>
2022-03-17 10:52:12 +0200

    Add files via upload

---
+++ GoodFood/Autentificare_clienti.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodFood
+{
+    public partial class Autentificare_clienti : Form
+    {
+        List<User> Users = new List<User>();
+        public Autentificare_clienti()
+        {
+            InitializeComponent();
+            btn_autentificare.BackColor = Color.SkyBlue;
+            btn_back.BackColor = Color.SkyBlue;
+            
+        }
+
+        private void btn_autentificare_Click(object sender, EventArgs e)
+        {
+            if (textBox_email.Text != "" && textBox_password.Text != "")
+            {
+                foreach(User user in Users)
+                    if (textBox_email.Text == user.email && textBox_password.Text == user.password)
+                    {
+                        this.Hide();
+                        Optiuni form = new Optiuni();
+                        form.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Eroare autentificare!");
+                        textBox_email.Clear();
+                        textBox_password.Clear();
+                    }
+            }
+            else
+                MessageBox.Show("Introduceti datele de autentificare!");
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            Start form = new Start();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ GoodFood/Creare_cont_succes.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace GoodFood
+{
+    partial class Creare_cont_succes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_succes = new System.Windows.Forms.Label();
+            this.button_succes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_succes
+            // 
+            this.label_succes.AutoSize = true;
+            this.label_succes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_succes.Location = new System.Drawing.Point(268, 187);
+            this.label_succes.Name = "label_succes";
+            this.label_succes.Size = new System.Drawing.Size(254, 24);
+            this.label_succes.TabIndex = 0;
+            this.label_succes.Text = "Contul a fost creat cu succes!";
+            // 
+            // button_succes
+            // 
+            this.button_succes.BackColor = System.Drawing.Color.SkyBlue;
+            this.button_succes.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button_succes.Location = new System.Drawing.Point(669, 392);
+            this.button_succes.Name = "button_succes";
+            this.button_succes.Size = new System.Drawing.Size(119, 46);
+            this.button_succes.TabIndex = 1;
+            this.button_succes.Text = "Autentificare";
+            this.button_succes.UseVisualStyleBackColor = false;
+            this.button_succes.Click += new System.EventHandler(this.button_succes_Click);
+            // 
+            // Creare_cont_succes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_succes);
+            this.Controls.Add(this.label_succes);
+            this.Name = "Creare_cont_succes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_succes;
+        private System.Windows.Forms.Button button_succes;
+    }
+}
